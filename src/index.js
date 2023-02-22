f8a559516cbb1ce5bb39f710aeab57a8eb1079ff
Paulo Henrique <ph.ouverney@gmail.com>
2023-02-22 15:06:36 -0300

    feat: new env to run locally; remove locked file for npm

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://server-production-1e4f.up.railway.app/', // change this to YOUR server URI
+  uri:
+    process.env.REACT_APP_GRAPHQL_URL ||
+    'https://server-production-1e4f.up.railway.app/', // change this to YOUR server URI
   cache: new InMemoryCache(),
   name: 'web',
   version: '1.0',
