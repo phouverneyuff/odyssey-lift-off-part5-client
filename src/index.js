0975445c447831fa4eca02f8cf0575be1b0b9da4
Paulo Henrique <ph.ouverney@gmail.com>
2023-02-22 14:14:32 -0300

    fix: change server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://odyssey-lift-off-part5-server-production.up.railway.app/', // change this to YOUR server URI
+  uri: 'https://server-production-1e4f.up.railway.app/', // change this to YOUR server URI
   cache: new InMemoryCache(),
   name: 'web',
   version: '1.0',
